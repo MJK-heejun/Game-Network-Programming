acc9ed0f7859922609b654dba1c0636a801d819f
hjk7 <hjk7@sfu.ca>
2017-02-05 22:41:10 -0800

    chat initial commit

---
+++ 4-Chat/Client1/Client1/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace client1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            TransportUDP udpLib = new TransportUDP();
+
+            udpLib.StartServer(14674,2);
+
+
+        }
+    }
+}
---
+++ 4-Chat/Client2/Client2/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace networkLibraryConsoleAppTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            TransportUDP udpLib = new TransportUDP();
+
+            udpLib.StartServer(14674,2);
+
+
+        }
+    }
+}
