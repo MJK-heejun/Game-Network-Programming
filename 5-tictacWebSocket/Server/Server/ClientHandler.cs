1411776f66d66fb2e764b638670d09b906a9b886
hjk7 <hjk7@sfu.ca>
2017-03-19 17:46:32 -0700

    websocketTcp.cs added

---
+++ 5-tictacWebSocket/Server/Server/ClientHandler.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+
+
+public class ClientHandler
+{
+    TcpClient clientSocket;
+    string clNo;
+
+    public void StartClient(TcpClient clientSocket, string clNo)
+    {
+        this.clientSocket = clientSocket;
+        this.clNo = clNo;
+        //start new thread for the client 'clineNo'
+        Thread ctThread = new Thread(doChat);
+        ctThread.Start();
+    }
+
+    private void doChat()
+    {
+        while ((true))
+        {
+            NetworkStream stream = clientSocket.GetStream();
+
+            while (!stream.DataAvailable) ;
+
+            Byte[] bytes = new Byte[clientSocket.Available];
+
+            stream.Read(bytes, 0, bytes.Length);
+
+            //translate bytes of request to string
+            String data = Encoding.UTF8.GetString(bytes);
+            Console.WriteLine(getDecodedData(bytes, bytes.Length) + ": hello from client handler");
+        }
+    }
+
+    private string getDecodedData(byte[] buffer, int length)
+    {
+        byte b = buffer[1];
+        int dataLength = 0;
+        int totalLength = 0;
+        int keyIndex = 0;
+
+        if (b - 128 <= 125)
+        {
+            dataLength = b - 128;
+            keyIndex = 2;
+            totalLength = dataLength + 6;
+        }
+
+        if (b - 128 == 126)
+        {
+            dataLength = BitConverter.ToInt16(new byte[] { buffer[3], buffer[2] }, 0);
+            keyIndex = 4;
+            totalLength = dataLength + 8;
+        }
+
+        if (b - 128 == 127)
+        {
+            dataLength = (int)BitConverter.ToInt64(new byte[] { buffer[9], buffer[8], buffer[7], buffer[6], buffer[5], buffer[4], buffer[3], buffer[2] }, 0);
+            keyIndex = 10;
+            totalLength = dataLength + 14;
+        }
+
+        if (totalLength > length)
+            throw new Exception("The buffer length is small than the data length");
+
+        byte[] key = new byte[] { buffer[keyIndex], buffer[keyIndex + 1], buffer[keyIndex + 2], buffer[keyIndex + 3] };
+
+        int dataIndex = keyIndex + 4;
+        int count = 0;
+        for (int i = dataIndex; i < totalLength; i++)
+        {
+            buffer[i] = (byte)(buffer[i] ^ key[count % 4]);
+            count++;
+        }
+
+        return Encoding.ASCII.GetString(buffer, dataIndex, dataLength);
+    }
+
+
+}
---
+++ 5-tictacWebSocket/Server/Server/WebSocketTCP.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections;
+using System.Net;
+using System.Net.Sockets;
+using System.Security.Cryptography;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading;
+
+
+public class WebSocketTCP
+{
+    TcpListener serverSocket;
+
+
+    // Use this for initialization
+    public WebSocketTCP(string ipAddr = "127.0.0.1", int port = 8888)
+    {
+        serverSocket = new TcpListener(IPAddress.Parse(ipAddr), 80);
+    }
+
+
+    // start server socket|
+    public void StartServer()
+    {
+        serverSocket.Start();
+        acceptClient();
+    }
+
+    private void acceptClient() {
+        while (true)
+        {
+            TcpClient clientSocket = serverSocket.AcceptTcpClient();
+            NetworkStream stream = clientSocket.GetStream();
+
+            while (!stream.DataAvailable) ;
+
+            Byte[] bytes = new Byte[clientSocket.Available];
+            stream.Read(bytes, 0, bytes.Length);
+
+            //translate bytes of request to string
+            String data = Encoding.UTF8.GetString(bytes);
+
+            if (new Regex("^GET").IsMatch(data))
+            {
+                handShake(stream, data);
+                //start new thread
+                ClientHandler client = new ClientHandler();
+                client.StartClient(clientSocket, "11111");
+            }
+        }
+    }
+
+    // stop server socket
+    public void StopServer()
+    {
+        serverSocket.Stop();
+    }
+
+
+
+    private void handShake(NetworkStream stream, String data)
+    {
+        Byte[] response = Encoding.UTF8.GetBytes("HTTP/1.1 101 Switching Protocols" + Environment.NewLine
+            + "Connection: Upgrade" + Environment.NewLine
+            + "Upgrade: websocket" + Environment.NewLine
+            + "Sec-WebSocket-Accept: " + Convert.ToBase64String(
+                SHA1.Create().ComputeHash(
+                    Encoding.UTF8.GetBytes(
+                        new Regex("Sec-WebSocket-Key: (.*)").Match(data).Groups[1].Value.Trim() + "258EAFA5-E914-47DA-95CA-C5AB0DC85B11"
+                    )
+                )
+            ) + Environment.NewLine
+            + Environment.NewLine);
+
+        stream.Write(response, 0, response.Length);
+    }
+   
+
+}
